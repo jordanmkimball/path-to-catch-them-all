29f7db2582c81a64980e8a91c0af14b16f0fa610
Jordan kimball <jordan.matthew.kimball@gmail.com>
2018-07-29 15:00:19 -0400

    Added text to the website homepage. Worked on the website CSS.

--- pokemon-path/routes/index.js
+++ pokemon-path/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Which games do I need to catch all the Pokémon?' });
 });
 
 module.exports = router;
