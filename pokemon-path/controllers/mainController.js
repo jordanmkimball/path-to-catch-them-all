039745d4dac846606ec74683906cb5390ef4044a
Jordan kimball <jordan.matthew.kimball@gmail.com>
2018-07-30 20:54:19 -0400

    Added routes to the web application. Installed Sqlite3

---
+++ pokemon-path/controllers/mainController.js
@@ -0,0 +1,18 @@
+//WHERE I will put requirements linked to the Sqlite3 database probably
+
+
+
+//Display Existing Players Page on GET
+exports.existing_get = function (req, res){
+    res.send('NOT IMPLIMENTED: existing_get page should be here');
+};
+
+//Display About Page
+exports.about_get = function (req, res){
+    res.send('NOT IMPLIMENTED: about_get page should be here');
+};
+
+//Display New Players Page
+exports.new_get = function (req, res){
+    res.send('NOT IMPLIMENTED: new_get should be here');
+}
--- pokemon-path/routes/index.js
+++ pokemon-path/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+//Require Controller Modules
+var main_controller = require('../controllers/mainController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Which games do I need to catch all the Pokémon?' });
 });
+
+//GET request for existing players page.
+router.get('/existing', main_controller.existing_get);
+
+//GET request for About page
+router.get('/about', main_controller.about_get);
+
+//GET request for new players page.
+router.get('/new', main_controller.new_get);
 
 module.exports = router;
