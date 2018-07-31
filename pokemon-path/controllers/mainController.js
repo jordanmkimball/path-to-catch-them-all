5cefbbf9eb83e0c1bfb4f4e93f2259af8839e38d
Jordan kimball <jordan.matthew.kimball@gmail.com>
2018-07-31 16:45:27 -0400

    Added a search Pokemon availability by Id function

--- pokemon-path/controllers/mainController.js
+++ pokemon-path/controllers/mainController.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
 //WHERE I will put requirements linked to the Sqlite3 database probably
-var sqlite3 = require('sqlite3');
+var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('pokemon.db');
 
 
@@ -37,12 +37,15 @@ exports.pokemon_search = function (req, res, next){
 //Display information about a single pokemon by id
 exports.pokemon_id_search = function (req, res, next){
     console.log("Return pokemon with Id: " + req.params.id);
-    db.get('SELECT * FROM AvPokemon WHERE id = ?', [req.params.id], function (err, row) {
-        if (err) {
-            res.send(err.message);
-        }
+    db.get('SELECT Id, Name, USun, UMoon FROM AvPokemon WHERE id = ?', [req.params.id], function (err, row) {
+        if (err) { res.send(err.message); }
+        //No error so render. 
         else{
-            res.json(row);
+            var pokedexNumber = 'Pokédex Number: ' + row.Id;
+            var pokemonName = 'Pokémon Name: ' + row.Name;
+            var ultraSun = 'Ultra Sun Availability: ' + row.USun;
+            var ultraMoon = 'Ultra Moon Availability: ' + row.UMoon;
+            res.render('pokemon_id_search', {title: 'Pokémon Availability Details', pokemon_id: pokedexNumber, pokemon_name: pokemonName, ultra_sun: ultraSun, ultra_moon: ultraMoon});
         }
     });
 };
